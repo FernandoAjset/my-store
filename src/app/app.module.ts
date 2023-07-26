ea76fe6d91f0b6894b139d438a0a4594cd0fea33
Ferajset-G3 <fernando_ajset@hotmail.com>
2023-07-25 20:46:41 -0600

    Primera Maquetacion De Proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductsComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent {
+  public products: Product[] = [
+    {
+      id: 1,
+      name: 'Smartwatch',
+      price: 200,
+      image: './assets/eCommerce/1.png',
+    },
+    {
+      id: 2,
+      name: 'SmartPhone',
+      price: 300,
+      image: './assets/eCommerce/2.png',
+    },
+    {
+      id: 1,
+      name: 'Monitor',
+      price: 320,
+      image: './assets/eCommerce/3.png',
+    },
+  ]
+}
